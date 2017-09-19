41046c28371f9e4b2b4f20de4591a7bc9f53bbd4
polar <polar@adiron.com>
2017-09-19 16:06:31 -0400

    Trying to modify run.sh so it waits for the Xvfb server to start accepting connections.

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 start-stop-daemon --start --pidfile ~/xvfb.pid --make-pidfile --background --exec /usr/bin/Xvfb -- :99 -screen 0 1024x768x24 -ac +extension GLX +render -noreset
-sleep 1
+
+timeout 15 bash -c 'until echo > /dev/tcp/localhost/99; do sleep 0.5; done'
 
 export DISPLAY=:99.0
 
