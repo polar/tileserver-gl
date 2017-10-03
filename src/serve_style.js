9b673638e3d3e50ed95c36d14c55976ceaadf9dd
Joseph Canero <jmc41493@gmail.com>
2017-10-03 15:30:37 -0400

    when serving styles, look in a whitelisted set of urls to also see when we can add the key parameter to the url. also allow configuration of the parameter name.

--- src/serve_style.js
+++ src/serve_style.js
@@ -1,25 +1,27 @@
 'use strict';
 
 var path = require('path'),
-    fs = require('fs');
+  fs = require('fs'),
+  nodeUrl = require('url'),
+  querystring = require('querystring');
 
 var clone = require('clone'),
-    express = require('express');
+  express = require('express');
 
 
-module.exports = function(options, repo, params, id, reportTiles, reportFont) {
+module.exports = function (options, repo, params, id, reportTiles, reportFont) {
   var app = express().disable('x-powered-by');
 
   var styleFile = path.resolve(options.paths.styles, params.style);
 
   var styleJSON = clone(require(styleFile));
-  Object.keys(styleJSON.sources).forEach(function(name) {
+  Object.keys(styleJSON.sources).forEach(function (name) {
     var source = styleJSON.sources[name];
     var url = source.url;
     if (url && url.lastIndexOf('mbtiles:', 0) === 0) {
       var mbtilesFile = url.substring('mbtiles://'.length);
       var fromData = mbtilesFile[0] == '{' &&
-                     mbtilesFile[mbtilesFile.length - 1] == '}';
+        mbtilesFile[mbtilesFile.length - 1] == '}';
 
       if (fromData) {
         mbtilesFile = mbtilesFile.substr(1, mbtilesFile.length - 2);
@@ -33,7 +35,7 @@ module.exports = function(options, repo, params, id, reportTiles, reportFont) {
     }
   });
 
-  styleJSON.layers.forEach(function(obj) {
+  styleJSON.layers.forEach(function (obj) {
     if (obj['type'] == 'symbol') {
       var fonts = (obj['layout'] || {})['text-font'];
       if (fonts && fonts.length) {
@@ -50,10 +52,10 @@ module.exports = function(options, repo, params, id, reportTiles, reportFont) {
   var httpTester = /^(http(s)?:)?\/\//;
   if (styleJSON.sprite && !httpTester.test(styleJSON.sprite)) {
     spritePath = path.join(options.paths.sprites,
-        styleJSON.sprite
-            .replace('{style}', path.basename(styleFile, '.json'))
-            .replace('{styleJsonFolder}', path.relative(options.paths.sprites, path.dirname(styleFile)))
-            );
+      styleJSON.sprite
+        .replace('{style}', path.basename(styleFile, '.json'))
+        .replace('{styleJsonFolder}', path.relative(options.paths.sprites, path.dirname(styleFile)))
+    );
     styleJSON.sprite = 'local://styles/' + id + '/sprite';
   }
   if (styleJSON.glyphs && !httpTester.test(styleJSON.glyphs)) {
@@ -62,28 +64,57 @@ module.exports = function(options, repo, params, id, reportTiles, reportFont) {
 
   repo[id] = styleJSON;
 
-  app.get('/' + id + '/style.json', function(req, res, next) {
-    var fixUrl = function(url, opt_nokey, opt_nostyle) {
-      if (!url || (typeof url !== 'string') || url.indexOf('local://') !== 0) {
+  var isWhitelistedUrl = function (url) {
+    if (!options.auth || !Array.isArray(options.auth.keyDomains) || options.auth.keyDomains.length === 0) {
+      return false;
+    }
+
+    for (var i = 0; i < options.auth.keyDomains.length; i++) {
+      var keyDomain = options.auth.keyDomains[i];
+      if (!keyDomain || (typeof keyDomain !== 'string') || keyDomain.length === 0) {
+        continue;
+      }
+
+      if (url.indexOf(keyDomain) === 0) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  app.get('/' + id + '/style.json', function (req, res, next) {
+    var fixUrl = function (url, opt_nokey, opt_nostyle) {
+      if (!url || (typeof url !== 'string') || (url.indexOf('local://') !== 0 && !isWhitelistedUrl(url))) {
         return url;
       }
-      var queryParams = [];
+
+      var queryParams = {};
       if (!opt_nostyle && global.addStyleParam) {
-        queryParams.push('style=' + id);
+        queryParams.style = id;
       }
-      if (!opt_nokey && req.query.key) {
-        queryParams.unshift('key=' + req.query.key);
+      if (!opt_nokey && req.query[options.auth.keyName]) {
+        queryParams[options.auth.keyName] = req.query[options.auth.keyName];
       }
-      var query = '';
-      if (queryParams.length) {
-        query = '?' + queryParams.join('&');
-      }
-      return url.replace(
+
+      if (url.indexOf('local://') === 0) {
+        var query = querystring.stringify(queryParams);
+        if (query.length) {
+          query = '?' + query;
+        }
+        return url.replace(
           'local://', req.protocol + '://' + req.headers.host + '/') + query;
+      } else { // whitelisted url. might have existing parameters
+        var parsedUrl = nodeUrl.parse(url);
+        var parsedQS = querystring.parse(url.query);
+        var newParams = Object.assign(parsedQS, queryParams);
+        parsedUrl.search = querystring.stringify(parsedQS);
+        return url.format(parsedUrl);
+      }
     };
 
     var styleJSON_ = clone(styleJSON);
-    Object.keys(styleJSON_.sources).forEach(function(name) {
+    Object.keys(styleJSON_.sources).forEach(function (name) {
       var source = styleJSON_.sources[name];
       source.url = fixUrl(source.url);
     });
@@ -98,24 +129,24 @@ module.exports = function(options, repo, params, id, reportTiles, reportFont) {
   });
 
   app.get('/' + id + '/sprite:scale(@[23]x)?\.:format([\\w]+)',
-      function(req, res, next) {
-    if (!spritePath) {
-      return res.status(404).send('File not found');
-    }
-    var scale = req.params.scale,
-        format = req.params.format;
-    var filename = spritePath + (scale || '') + '.' + format;
-    return fs.readFile(filename, function(err, data) {
-      if (err) {
-        console.log('Sprite load error:', filename);
+    function (req, res, next) {
+      if (!spritePath) {
         return res.status(404).send('File not found');
-      } else {
-        if (format == 'json') res.header('Content-type', 'application/json');
-        if (format == 'png') res.header('Content-type', 'image/png');
-        return res.send(data);
       }
+      var scale = req.params.scale,
+        format = req.params.format;
+      var filename = spritePath + (scale || '') + '.' + format;
+      return fs.readFile(filename, function (err, data) {
+        if (err) {
+          console.log('Sprite load error:', filename);
+          return res.status(404).send('File not found');
+        } else {
+          if (format == 'json') res.header('Content-type', 'application/json');
+          if (format == 'png') res.header('Content-type', 'image/png');
+          return res.send(data);
+        }
+      });
     });
-  });
 
   return Promise.resolve(app);
 };
